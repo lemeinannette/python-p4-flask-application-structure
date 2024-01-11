18ef4a1e5d9b05f3b742789cb966fc8ab6dd3089
Lemein <lemeinannette@gmail.com>
2024-01-11 12:29:38 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(port=5555)
+
+@app.route('/')
+def index():
+    return'<h1>Welcome to my app !</h1>'
+@app.route('/<username>')
+def user(username):
+    return f'<h1>profile for {username}</h1>'
+@app.route('/string:lemein>')
+def my_name (lemein):
+    return f'<h1>profile for {lemein}</h1>'
